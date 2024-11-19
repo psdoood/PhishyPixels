e3754f60db001bb63ca42fc9aab80eac2bc4df6d
psdoood <aydeniswright@gmail.com>
2024-11-18 19:04:54 -0700

    Removed unneeded imports and cleaned up a few comments that were no longer accurate.

--- data_processing.py
+++ data_processing.py
@@ -1,24 +1,15 @@
 import os
 import numpy as np
 import colorgram as cg
-import pytesseract
 
-#For windows:
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-#For Linux (for use in some testing I did)
-#pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
-
-#List of some of the more popular targeted websites to focus on
+#List targeted brands I am focusing on
 BRAND_NAMES = ['facebook', 'netflix', 'microsoft', 'tiktok', 'amazon', 'paypal', 'instagram', 'steam', 'apple', 'whatsapp']
 NUM_BRAND_FEATURES = len(BRAND_NAMES)
 #How many colors to extract from each image
 NUM_COLORS = 3
-#How many features should be present in each data structure
-EXPECTED_FEATURES = NUM_COLORS + NUM_BRAND_FEATURES + 1 
+EXPECTED_FEATURES = NUM_COLORS + NUM_BRAND_FEATURES + 1 # + 1 is for phish_val
 
-#------------------------------------------------------------------------------------------------------#
-
-#Extracts the 5 most dominant colors from each screenshot
+#Extracts the (NUM_COLORS) most dominant colors from each screenshot
 def extract_colors(screenshot):
     hex_color_list = []
     colors  = cg.extract(screenshot, NUM_COLORS)
@@ -28,15 +19,13 @@ def extract_colors(screenshot):
         hex_val = float((color.rgb[0] << 16) + (color.rgb[1] << 8) + color.rgb[2]) / float(0xFFFFFF)
         hex_color_list.append(hex_val)
         
-    #If there is less than NUM_COLORS in the screenshot, add dummy color
+    #If there is less than NUM_COLORS in the screenshot, add dummy color (0.0)
     while len(hex_color_list) < NUM_COLORS :
         hex_color_list.append(0.0)
 
     return hex_color_list
 
-#------------------------------------------------------------------------------------------------------#
-
-#Creates the data strucutres [(colors), (brands), (0 or 1 <0 is phish, 1 is not>)]
+#Creates the data strucutres [(colors) + (brands) + (0 or 1 <0 is phish, 1 is not>)]
 def create_data_structure(screenshots_paths, brand_index, is_phish):
     data = []
     if is_phish == True:
@@ -47,7 +36,8 @@ def create_data_structure(screenshots_paths, brand_index, is_phish):
     for path in screenshots_paths:
         colors = extract_colors(path)
 
-        #Each brand has its own index in the data structure, so the values are just 0 or 1
+        #Each brand has its own index related to BRAND_NAMES, values are binary
+        #0 mean brand not present, 1 means brand is present
         brand_features = [0] * len(BRAND_NAMES)
         brand_features[brand_index] = 1
         features = colors + brand_features + [val]
@@ -63,5 +53,5 @@ def create_data_structure(screenshots_paths, brand_index, is_phish):
         #Return the data in the correct array shape
         return data_array.reshape(len(data), EXPECTED_FEATURES)
     else:
-        #Return an empty array if data wasnt created
+        #Return an empty array if data wasn't created
         return np.array(data, dtype=float).reshape(0, EXPECTED_FEATURES)
